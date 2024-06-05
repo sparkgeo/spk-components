225d486ee9e6c78ff136a23c74b0cb1818521637
Dave Dowding <ddowding@sparkgeo.com>
2024-06-05 15:42:34 +0100

    fix: improved code format

--- src/components/core/BinnedLegend.jsx
+++ src/components/core/BinnedLegend.jsx
@@ -3,64 +3,61 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./BinnedLegend.css";
 
+const Bin = ({ color, label }) => (
+    <div key={label} className="bin flex-container">
+        <span className="color-box" style={{ backgroundColor: color }} />
+        <span className="label">{label}</span>
+    </div>
+);
+
+Bin.propTypes = {
+    color: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+};
+
+const layouts = {
+    horizontal: (bins) => (
+        <div className="binned-numbers horizontal">
+            {bins.map((bin) => (
+                <Bin color={bin.color} label={bin.label} />
+            ))}
+        </div>
+    ),
+    vertical: (bins) => (
+        <div className="binned-numbers vertical">
+            {bins.map((bin) => (
+                <Bin color={bin.color} label={bin.label} />
+            ))}
+        </div>
+    ),
+    grid: (bins) => (
+        <div className="binned-legend">
+            <div className="color-row">
+                {bins.map((bin) => (
+                    <div key={bin.label} className="grid-item">
+                        <span
+                            className="grid-color-box"
+                            style={{ backgroundColor: bin.color }}
+                        />
+                        <span className="label">{bin.label}</span>
+                    </div>
+                ))}
+            </div>
+        </div>
+    ),
+};
+
 export const BinnedLegend = ({
     title = "Binned Legend",
     titleBelow = false,
     displayType = "horizontal",
     bins,
 }) => {
-    let layout;
-    switch (displayType) {
-        case "horizontal":
-            layout = (
-                <div className="binned-numbers horizontal">
-                    {bins.map((bin) => (
-                        <div key={bin.label} className="bin flex-container">
-                            <span
-                                className="color-box"
-                                style={{ backgroundColor: bin.color }}
-                            />
-                            <span className="label">{bin.label}</span>
-                        </div>
-                    ))}
-                </div>
-            );
-            break;
-        case "vertical":
-            layout = (
-                <div className="binned-numbers vertical">
-                    {bins.map((bin) => (
-                        <div key={bin.label} className="bin flex-container">
-                            <span
-                                className="color-box"
-                                style={{ backgroundColor: bin.color }}
-                            />
-                            <span className="label">{bin.label}</span>
-                        </div>
-                    ))}
-                </div>
-            );
-            break;
-        case "grid":
-            layout = (
-                <div className="binned-legend">
-                    <div className="color-row">
-                        {bins.map((bin) => (
-                            <div key={bin.label} className="grid-item">
-                                <span
-                                    className="grid-color-box"
-                                    style={{ backgroundColor: bin.color }}
-                                />
-                                <span className="label">{bin.label}</span>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            );
-            break;
-        default:
-            layout = <div>Invalid displayType</div>;
-    }
+    const layout = layouts[displayType]
+        ? layouts[displayType](bins)
+        : (() => {
+              throw new Error(`Invalid displayType: ${displayType}`);
+          })();
 
     return (
         <div className="binned-box">
@@ -74,6 +71,11 @@ export const BinnedLegend = ({
 BinnedLegend.propTypes = {
     title: PropTypes.string,
     titleBelow: PropTypes.bool,
-    bins: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired,
+    bins: PropTypes.arrayOf(
+        PropTypes.shape({
+            color: PropTypes.string.isRequired,
+            label: PropTypes.string.isRequired,
+        }),
+    ).isRequired,
     displayType: PropTypes.oneOf(["horizontal", "vertical", "grid"]),
 };
