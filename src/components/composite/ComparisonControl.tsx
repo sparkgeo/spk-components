178044ed754507e1c389e67e0e239ad8a5284506
Adam Tweedie <atweedie@sparkgeo.com>
2024-12-04 13:17:02 +0000

    chore: adds generated JSDoc snippet for ComparisonControl

--- src/components/composite/ComparisonControl.tsx
+++ src/components/composite/ComparisonControl.tsx
@@ -13,6 +13,24 @@ export interface BaseComparisonControlProps
     value?: number;
 }
 
+/**
+ * Props for the `ComparisonControl` component.
+ * 
+ * @interface BaseComparisonControlProps
+ * @extends {AriaSliderProps}
+ * @property {ReactNode | string} label - The label for the slider.
+ * @property {boolean} [showLabel] - Whether to show the label. Defaults to true.
+ * @property {number} [value] - The initial value of the slider. Defaults to 50.
+ * @property {(value: number | number[]) => void} onChange - Callback function called when the slider value changes.
+ */
+
+/**
+ * A composite component that renders a slider control with optional label and custom thumb icon.
+ * 
+ * @param {BaseComparisonControlProps} props - The props for the component.
+ * @returns {JSX.Element} The rendered slider control component.
+ */
+
 export const ComparisonControl = ({
     label,
     showLabel = true,
