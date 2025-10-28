deb29fce42e06f4befab2a92886f8f08744ff9c6
Adam Tweedie <atweedie@sparkgeo.com>
2025-10-28 15:22:49 +0000

    fix: refactors layerToggle to improve style implementation

--- src/components/core/LayerToggle.jsx
+++ src/components/core/LayerToggle.jsx
@@ -1,23 +1,17 @@
 import PropTypes from "prop-types";
 import React from "react";
+import { Switch } from "react-aria-components";
 import styles from "./LayerToggle.module.css";
 
-export const LayerToggle = ({ layerName, isActive = false, onChange }) => (
-    <div className={styles.layerToggle}>
-        <div className={styles.switch}>
-            <input
-                type="checkbox"
-                value={layerName}
-                checked={isActive}
-                onChange={onChange}
-            />
-            <span className={styles.slider} />
-        </div>
-    </div>
+export const LayerToggle = ({ layerName, isActive = false, onChange, ...props }) => (
+    <Switch {...props} className={styles.switch} isSelected={isActive} onChange={onChange} aria-label={layerName} value={layerName}>
+        <div className={styles.indicator}/>
+    </Switch>
 );
 
 LayerToggle.propTypes = {
     layerName: PropTypes.string,
     isActive: PropTypes.bool,
     onChange: PropTypes.func.isRequired,
+    disabled: PropTypes.bool
 };
